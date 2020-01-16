28f4da656e0beb319d59f6f9fa0d34de0565ede1
nn <nn@nn-PC>
2020-01-16 08:31:43 +0530

    Commit Test Commit Webdriver

---
+++ src/test/java/webdriver/tests/WebdriverHandleTest.java
@@ -0,0 +1,22 @@
+package webdriver.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class WebdriverHandleTest {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Hello" , true);
+	    System.setProperty("webdriver.gecko.driver", "../MavenProject/exe/geckodriver.exe");
+	    WebDriver driver = new FirefoxDriver();       
+	    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	    driver.navigate().to("http://www.google.com");
+	}
+
+}
