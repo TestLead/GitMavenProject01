c789d2936073497d0c2d9bd36837c6212ac83858
nn <nn@nn-PC>
2020-01-16 10:02:35 +0530

    Updated Commit

---
+++ src/test/java/tests/DemoTest2.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	 @Test
+	  public void openMyBlog() {
+		Reporter.log("HELLO As an TE1 updated code in TE1 terminal" , true);
+		Reporter.log("HELLO I am testing Maven" , true);
+		System.out.println("I AM TESTING MAVEN PROJECT********************");
+	  }
+	  
+
+}
